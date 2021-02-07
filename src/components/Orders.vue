b07a3f640952d33a1c3fb9166c58dd69c0cb5b11
“Annika <annika.parm@vikk.ee>
2021-02-07 19:11:11 +0200

    0702

--- src/components/Orders.vue
+++ src/components/Orders.vue
@@ -1,17 +1,51 @@
 <template>
   <div>
     <h2 class="mb-3">Tellimused</h2>
+    <!-- <p>Count: {{ count }}</p>
+    <button @click="addCount()">+</button>
+    <button @click="removeCount()">-</button> -->
+
+    <b-table striped hover :items="items" :fields="fields"></b-table>
+    
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Orders',
   data() {
     return {
+      count: 0,
+      fields: ['first_name', 'last_name', 'age'],
+      items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
     }
   },
   async created () {
+    const orders = await axios({
+      url: 'api/orders',
+      method: 'GET',
+      headers: {}
+    })
+    console.log('orders', orders)
+
+    this.item = orders.data
+  },
+  methods: {
+    addCount () {
+      console.log('Praegune count:', this.count)
+      this.count++
+    },
+    removeCount () {
+      console.log('Praegune count:', this.count)
+      this.count--
+    }  
   }
 }
 </script>
